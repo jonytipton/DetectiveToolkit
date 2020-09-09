dae37fc3e3280c14a82ecffc8df86f29a6964c1e
Jonathan Titpon <jonathantipton@u.boisestate.edu>
2020-09-09 17:47:04 -0600

    Initial Commit

---
+++ DetectiveToolkit/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  DetectiveToolkit
+//
+//  Created by Jonathan Tipton on 9/9/20.
+//  Copyright © 2020 Jonathan Tipton. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
